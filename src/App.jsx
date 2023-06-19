7796985f2e87697b1c8541907f0ea5663b414179
Eduardo Tonatto <edtonatto@gmail.com>
2023-06-19 15:55:55 -0300

    :construction: Added code for sidebar profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { Header } from './components/Header'
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>Teste</main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1538481199705-c710c4e965fc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=300&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.akamai.steamstatic.com/b370c0241b976647aa27a5b90c147dc4cbd9eb00_full.jpg"
+        />   
+        <strong>Tonatto ★</strong>
+        <span>🇧🇷 Chapeco, Santa Catarina, Brazil</span>
+      </div>
+
+      <footer>
+        <a href="#">View Profile</a>
+      </footer>
+    </aside> 
+  )
+}
